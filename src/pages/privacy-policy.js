02e9b70c445c55076292a24fe52bbab2285a0159
Melvin Musehani <melvin@appchemy.co.za>
2020-04-23 15:10:14 +0200

    Added legal pages

---
+++ src/pages/privacy-policy.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import PageLayout from '../templates/page-layout'
+
+export default () => (
+    <div>
+        <PageLayout title='Privacy Policy' active='privacy-policy'>
+            
+        </PageLayout>
+    </div>
+)
---
+++ src/pages/terms-of-use.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import PageLayout from '../templates/page-layout'
+
+export default () => (
+    <div>
+        <PageLayout title='Terms of Use' active='terms-of-use'>
+            
+        </PageLayout>
+    </div>
+)
