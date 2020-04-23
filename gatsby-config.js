7c0542beb6a7633c9c26067f5d3bd97cf0082ce1
Melvin Musehani <melvin@appchemy.co.za>
2020-04-23 16:25:47 +0200

    Fix netlify plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,6 +6,17 @@
 
 module.exports = {
   /* Your site config here */
+  siteMetadata: {
+    title: 'ReactFire',
+    author: {
+      name: 'Melvin Musehani'
+    },
+    siteUrl: 'https://www.reactfire.com',
+    social: {
+      twitter: 'reactfire',
+      facebook: 'reactfire'
+    }
+  },
   plugins: [
     `gatsby-plugin-netlify-cms`
   ],
