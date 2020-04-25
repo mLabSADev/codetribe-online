9b8a40cc46af1ef152aa5b2317afbac961df5e33
Melvin Musehani <melvin@appchemy.co.za>
2020-04-25 04:20:58 +0200

    Fix padding issues

--- src/templates/blog.js
+++ src/templates/blog.js
@@ -2,8 +2,8 @@ import React from 'react'
 import { graphql } from "gatsby"
 import PageLayout from './page-layout'
 import Img from "gatsby-image"
-import { Row, Col, Divider, Space } from 'antd'
-import { CalendarFilled, UserOutlined, CalendarOutlined } from '@ant-design/icons'
+import { Divider, Space } from 'antd'
+import { UserOutlined, CalendarOutlined } from '@ant-design/icons'
 import PostListing from '../components/post-listing'
 
 export default ({ data }) => {
@@ -11,15 +11,11 @@ export default ({ data }) => {
 
     return (
         <div>
-            <PageLayout active={post.fields.type}>
-                <Row>
-                    <Col xs={0} md={2} lg={4}></Col>
-                    <Col xs={24} md={20} lg={16}>
+            <PageLayout active={post.fields.type} withPadding={false}>
                         <div style={{background: 'white'}}>
                             <Img alt={post.frontmatter.title} style={{
                                 width: '100%',
-                                marginBottom: 0,
-                                marginTop: 40
+                                marginBottom: 0
                             }} sizes={post.frontmatter.featureImage.childImageSharp.sizes} />
 
                             <div style={{display: 'flex', alignItems: 'center', flexDirection: 'column', paddingLeft: 40, paddingRight: 40}}>
@@ -41,20 +37,14 @@ export default ({ data }) => {
                             </div>
                             <div style={{fontSize: '1.2em', paddingLeft: 40, paddingRight: 40, paddingBottom: 40}} dangerouslySetInnerHTML={{ __html: post.html }} />
                         </div>
-                    </Col>
-                    <Col xs={0} md={2} lg={4}></Col>
-                </Row>
-                
-                <Row style={{marginTop: 40}}>
-                    <Col xs={0} md={2} lg={4}></Col>
-                    <Col xs={24} md={20} lg={16}>
-                        <div style={{background: 'white', padding: 40}}>
+
+                        <Divider style={{height: 40}} />
+
+                        <div style={{background: 'white', paddingLeft: 40, paddingRight: 40, paddingTop: 20}}>
                             <h2>More Posts</h2>
-                            <PostListing type='tutorials' limit={4} />
+                            <PostListing type='tutorials' limit={3} />
                         </div>
-                    </Col>
-                    <Col xs={0} md={2} lg={4}></Col>
-                </Row>
+
                 
             </PageLayout>
         </div>
--- src/templates/page-layout.js
+++ src/templates/page-layout.js
@@ -4,28 +4,28 @@ import Header from "../components/header"
 import Footer from "../components/footer"
 import '../styles/page-layout.css'
 
-const PageLayout = ({children, title, active, header}) => {
+const PageLayout = ({ children, title, active, header, withPadding = true }) => {
     return (
         <Layout>
-            <Layout.Header style={{background: 'white'}}>
+            <Layout.Header style={{ background: 'white' }}>
                 <Header active={active} />
             </Layout.Header>
             <Layout.Content>
 
-            <div>
-                {header}
-            </div>
-            <Row style={{marginTop: 40}}>
+                <div>
+                    {header}
+                </div>
+                <Row style={{ marginTop: 40 }}>
                     <Col xs={0} md={2} lg={4}></Col>
                     <Col xs={24} md={20} lg={16}>
-                <div style={{background: 'white', marginBottom: 40, minHeight: 280, paddingTop: 20, paddingBottom: 20, paddingLeft: 40, paddingRight: 40}}>
-                    {title && <h1>{title}</h1>}
-                    {children}
-                </div>
-                </Col>
+                        <div style={{ background: 'white', marginBottom: 40, minHeight: 280, paddingTop: withPadding ? 40 : 0, paddingBottom: 20, paddingLeft: withPadding ? 40 : 0, paddingRight: withPadding ? 40 : 0 }}>
+                            {title && <h1 style={{marginTop: 0, paddingTop: 0}}>{title}</h1>}
+                            {children}
+                        </div>
+                    </Col>
                 </Row>
             </Layout.Content>
-            <Layout.Footer style={{background: '#101010'}}>
+            <Layout.Footer style={{ background: '#101010' }}>
                 <Footer />
             </Layout.Footer>
         </Layout>
