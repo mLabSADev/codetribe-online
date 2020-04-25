d4e5d8a3112d1ee87da743040a0561214f58906a
Melvin Musehani <melvin@appchemy.co.za>
2020-04-25 21:20:42 +0200

    Add logo

--- src/components/header.js
+++ src/components/header.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Menu } from 'antd'
-import { navigate } from 'gatsby'
+import { navigate, Link } from 'gatsby'
 
 const Header = ({active}) => {
     const goToPage = ({key}) => {
@@ -14,7 +14,7 @@ const Header = ({active}) => {
     return (
         <div style={{display: 'flex'}}>
             <div style={{marginRight: 20}}>
-                ReactFire
+                <Link to='/'><img alt='ReactFire logo' src='/images/logo.png' style={{height: 40, marginRight: 40}} /></Link>
             </div>
             <Menu theme="light" mode="horizontal" selectedKeys={[active]} onSelect={goToPage}>
                 <Menu.Item key='home'>Home</Menu.Item>
