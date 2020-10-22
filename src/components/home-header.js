d3226b85de215961cc52882fb4bbda251ef9cae7
Melvin Musehani <melvin@appchemy.co.za>
2020-10-22 23:27:26 +0200

    Welcome to codetribe text

--- src/components/home-header.js
+++ src/components/home-header.js
@@ -11,7 +11,8 @@ export const HomeHeader = () => {
     return (
         <div id='home-header'>
             <h1>Learn how to build Android &amp; iOS Apps</h1>
-            <p>Building apps is easier than you think. We will take you through the process of building your own apps using React and Firebase</p>
+            <h2>Welcome to <span style={{fontWeight: 'bold'}}><span style={{color: "#97CA42"}}>code</span><span style={{color: "#9397A1"}}>tribe</span></span></h2>
+            <p>Building apps is easier than you think. We will take you through the process of building your own apps using Ionic and Firebase</p>
             <Button size='large' type='primary' onClick={getStarted}>Get Started</Button>
         </div>
     )
